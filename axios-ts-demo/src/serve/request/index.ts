8602b42d9a250bd5fa95a0ed36540513bc2734be
‘yues’ <434375527@qq.com>
2023-04-29 04:52:10 +0800

    feat: 全局请求拦截配置

--- axios-ts-demo/src/serve/request/index.ts
+++ axios-ts-demo/src/serve/request/index.ts
@@ -3,21 +3,43 @@ import axios from "axios";
 
 class AxiosRequest {
   instance: AxiosInstance;
-  constructor(config:any) {
+  constructor(config: any) {
     this.instance = axios.create(config);
+
+    this.instance.interceptors.request.use(
+      (config) => {
+        console.log("全局请求成功");
+        return config;
+      },
+      (err) => {
+        console.log("全局请求失败");
+        return err;
+      }
+    );
+
+    this.instance.interceptors.response.use(
+      (config) => {
+        console.log("全局响应成功");
+        return config;
+      },
+      (err) => {
+        console.log("全局响应失败");
+        return err;
+      }
+    );
   }
 
-  request(config:any){
+  request(config: any) {
     return this.instance.request(config);
   }
 
-  get(config:any){
-    this.request({...config,method:'get'})
+  get(config: any) {
+    this.request({ ...config, method: "get" });
   }
 
-  post(config:any){
-    this.request({...config,method:'post'})
+  post(config: any) {
+    this.request({ ...config, method: "post" });
   }
 }
 
-export default AxiosRequest
+export default AxiosRequest;
